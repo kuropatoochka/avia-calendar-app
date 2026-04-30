ba640ec9513b697fd9170657efbf033b1c98891d
iamqt <masha@masha.com>
2026-04-30 13:58:14 +0300

    feat: добавила форму поиска с обязательными полями

--- frontend/src/pages/OfferPage/ui/OfferPage.tsx
+++ frontend/src/pages/OfferPage/ui/OfferPage.tsx
@@ -1,9 +1,11 @@
+import { SearchForm } from './SearchForm';
 import { TitleBlock } from './TitleBlock';
 
 const OfferPage = () => {
   return (
     <>
       <TitleBlock />
+      <SearchForm />
     </>
   );
 };
