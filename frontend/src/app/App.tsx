6cf5798b54f791e5690587e71bdf9c48a0d0f94a
kuropatoochka <valya000999@gmail.com>
2026-05-23 00:36:22 +0300

    feat: add tracking quiz

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,6 +1,6 @@
 import { LaunchExperimentProvider } from '@/features/launch-experiment';
 import { ConfigProvider } from 'antd';
-import ruRU from 'antd/es/locale/ru_RU';
+import ruRU from 'antd/locale/ru_RU';
 import dayjs from 'dayjs';
 import 'dayjs/locale/ru';
 import { StrictMode } from 'react';
